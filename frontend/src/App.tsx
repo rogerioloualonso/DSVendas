8236beff25840c67d5166d7db45cd57919849ccc
Roger <rogerio.alonso@soulasalle.com.br>
2021-09-06 21:53:17 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import DataTable from "assets/components/DataTable";
+import Footer from "assets/components/Footer";
+import NavBar from "assets/components/NavBar";
 
 
 function App() {
   return (
-    <div>
-        <h1 className="text-primary">Olá mundo!</h1>
-    </div>
+    <>
+        <NavBar />
+        <div className="container">
+            <h1 className="text-primary">Olá mundo!</h1>
+            <DataTable/>
+        </div>
+        <Footer/>
+    </>
   );
 }
 
