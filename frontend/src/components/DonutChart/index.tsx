365199b11c0d6aaf660f06523aedf429425d4386
Roger <rogerio.alonso@soulasalle.com.br>
2021-09-12 13:02:22 -0300

    First request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,12 +1,33 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sale';
+import { BASE_URL } from 'utils/request';
 
-function DonutChart() {
+type ChartData = {
+    labels: string[];
+    series: number[];
+}
 
+function DonutChart(){
+
+    //ERRADO
+    let chartData : ChartData = { labels: [], series: []};
+
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+         .then(response => {
+            const data = response.data as SaleSum[];
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => x.sum);
+
+            chartData = { labels: myLabels, series: mySeries};
+            console.log(response.data);
+         })
+    /*
     const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     }
-    
+    */
     const options = {
         legend: {
             show: true
@@ -16,8 +37,8 @@ function DonutChart() {
 
     return (
         <Chart 
-            options={{ ...options, labels: mockData.labels}}
-            series={mockData.series}
+            options={{ ...options, labels: chartData.labels}}
+            series={chartData.series}
             type="donut"
             height="240"
         />
