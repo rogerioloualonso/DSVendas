61912d359c49f230faa048c6cb6848d943bafdfc
Roger <rogerio.alonso@soulasalle.com.br>
2021-09-12 11:47:43 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,10 @@
-import BarChart from "assets/components/BarChart";
-import DataTable from "assets/components/DataTable";
-import DonutChart from "assets/components/DonutChart";
-import Footer from "assets/components/Footer";
-import NavBar from "assets/components/NavBar";
-
+import Routes from "Routes";
 
 function App() {
   return (
-    <>
-        <NavBar />
-        <div className="container">
-            <h1 className="text-primary py-3">Dashboard de vendas</h1>
 
-            <div className="row px-3">
-                <div className="col-sm-6">
-                    <h5>Taxa de sucesso(%)</h5>
-                    <BarChart />
-                </div>
-                <div className="col-sm-6">
-                    <h5>Todas vendas</h5>
-                    <DonutChart />
-                </div>
-            </div>
-
-            <div className="py-3">
-                <h2 className="text-primary">Todas vendas</h2>
-            </div>
-
-            <DataTable/>
-        </div>
-        <Footer/>
-    </>
+    <Routes />
+    
   );
 }
 
